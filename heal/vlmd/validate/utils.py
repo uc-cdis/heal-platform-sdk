6363b3f5496f00941526e76a23f18d460b7e1c37
George Thomas <george42@uchicago.edu>
2024-10-23 16:08:42 -0700

    (HP-1726): use snake case for variables

--- heal/vlmd/validate/utils.py
+++ heal/vlmd/validate/utils.py
@@ -1,5 +1,4 @@
 import charset_normalizer
-import jsonschema
 import pandas as pd
 from pathlib import Path
 
@@ -27,7 +26,7 @@ def detect_file_encoding(file_path):
     return encoding_for_input["encoding"]
 
 
-def read_delim(file_path, castdtype="string"):
+def read_delim(file_path, cast_d_type="string"):
     """
     reads in a tabular file (ie spreadsheet) after detecting
     encoding and file extension without any type casting.
@@ -47,7 +46,7 @@ def read_delim(file_path, castdtype="string"):
 
     encoding = detect_file_encoding(file_path)
     file_encoding = pd.read_csv(
-        file_path, sep=sep, encoding=encoding, dtype=castdtype, keep_default_na=False
+        file_path, sep=sep, encoding=encoding, dtype=cast_d_type, keep_default_na=False
     )
 
     return file_encoding
@@ -63,7 +62,7 @@ def get_schema(input_file: str, schema_type: str):
         schema_type (str): the type of the schema that can be validated against.
             Allowed values for now are “csv”, “tsv”, “json” and “auto”. Defaults to “auto”
     Returns:
-        schema
+        schema (dict)
     """
 
     file_suffix = Path(input_file).suffix.replace(".", "")
@@ -71,33 +70,33 @@ def get_schema(input_file: str, schema_type: str):
         schema = {"type": "array", "items": CSV_SCHEMA}
         logger.debug("Validator will use CSV schema")
         return schema
-    elif schema_type == "tsv" or (schema_type == "auto" and file_suffix == "tsv"):
+    if schema_type == "tsv" or (schema_type == "auto" and file_suffix == "tsv"):
         schema = {"type": "array", "items": CSV_SCHEMA}
         logger.debug("Validator will use CSV schema for TSV file")
         return schema
-    elif schema_type == "json" or (schema_type == "auto" and file_suffix == "json"):
+    if schema_type == "json" or (schema_type == "auto" and file_suffix == "json"):
         schema = JSON_SCHEMA
         logger.debug("Validator will use json schema")
         return schema
-    else:
-        return None
+
+    return None
 
 
-def add_missing_type(propname: str, prop, schema: dict):
+def add_missing_type(prop_name: str, prop, schema: dict):
     """
     Add types to properties.
 
     Args:
-        propname (str): property name
+        prop_name (str): property name
         prop (str or dict): property
         schema (dict): schema
 
     Returns:
-        schema
+        schema (dict)
 
     """
     missing_values = ["", None]  # NOTE: include physical rep and logical for now
-    if propname in schema.get("required", []):
+    if prop_name in schema.get("required", []):
         # if required value: MUST be NOT missing value and the property
         newprop = {"allOf": [prop, {"not": {"enum": missing_values}}]}
     else:
@@ -111,21 +110,23 @@ def add_types_to_props(schema: dict) -> dict:
     Add missing types to the schema for validating csv style data.
 
     Args:
-        schema
+        schema (dict)
 
     Returns:
-        schema
+        schema (dict)
     """
 
     props_with_missing = {}
-    for propname, prop in schema.get("items", {}).get("properties", {}).items():
-        props_with_missing[propname] = add_missing_type(propname, prop, schema)
+    for prop_name, prop in schema.get("items", {}).get("properties", {}).items():
+        props_with_missing[prop_name] = add_missing_type(prop_name, prop, schema)
 
     patterns_with_missing = {}
-    for patternname, prop in (
+    for pattern_name, prop in (
         schema.get("items", {}).get("patternProperties", {}).items()
     ):
-        patterns_with_missing[patternname] = add_missing_type(patternname, prop, schema)
+        patterns_with_missing[pattern_name] = add_missing_type(
+            pattern_name, prop, schema
+        )
 
     schema = {"type": "array", "items": {}}
     schema["items"]["properties"] = props_with_missing
