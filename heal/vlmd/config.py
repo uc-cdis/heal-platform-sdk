10e4a56f61884887ca22254f54677166d96c3cf0
George Thomas <george42@uchicago.edu>
2024-10-29 12:49:07 -0700

    (HP-1727): update config for extract

--- heal/vlmd/config.py
+++ heal/vlmd/config.py
@@ -1,8 +1,14 @@
 import json
 
+# file prefix
+OUTPUT_FILE_PREFIX = "heal-dd"
+
+# file suffixes
 ALLOWED_INPUT_TYPES = ["csv", "tsv", "json"]
+ALLOWED_FILE_TYPES = ["auto"] + ALLOWED_INPUT_TYPES
 ALLOWED_SCHEMA_TYPES = ["auto", "csv", "json", "tsv"]
 
+# schemas
 csv_schema_file = "heal/vlmd/schemas/heal_csv.json"
 with open(csv_schema_file, "r") as f:
     CSV_SCHEMA = json.load(f)
@@ -10,3 +16,9 @@ with open(csv_schema_file, "r") as f:
 json_schema_file = "heal/vlmd/schemas/heal_json.json"
 with open(json_schema_file, "r") as f:
     JSON_SCHEMA = json.load(f)
+
+# schema
+TOP_LEVEL_PROPS = {
+    "schemaVersion": JSON_SCHEMA.get("version", "0.3.2"),
+    "title": "HEAL Data Dictionary",
+}
