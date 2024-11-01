32d72813144b5aed355c4ac16658415c58e16e2d
George Thomas <george42@uchicago.edu>
2024-11-01 14:30:33 -0700

    (HP-1727): add unit test for file writing

--- tests/test_vlmd_file_utils.py
+++ tests/test_vlmd_file_utils.py
@@ -1,5 +1,42 @@
+import json
+import os
+
+import pandas as pd
+
 from heal.vlmd.config import OUTPUT_FILE_PREFIX
 from heal.vlmd.file_utils import get_output_filepath, write_vlmd_dict
+
+
+def test_write_json(tmp_path, valid_json_data):
+    output_filepath = f"{tmp_path}/some/path/valid_dict.json"
+    if os.path.isfile(output_filepath):
+        os.unlink(output_filepath)
+    result = write_vlmd_dict(
+        valid_json_data, output_filepath=output_filepath, file_type="json"
+    )
+    assert result
+
+    with open(output_filepath, "r") as f:
+        data = json.load(f)
+    assert data == valid_json_data
+
+
+def test_write_csv(tmp_path, valid_array_data):
+    output_filepath = f"{tmp_path}/some/other/path/valid_dict.csv"
+    if os.path.isfile(output_filepath):
+        os.unlink(output_filepath)
+    result = write_vlmd_dict(
+        valid_array_data, output_filepath=output_filepath, file_type="csv"
+    )
+    assert result
+
+    # reconstruct the array_data from the written csv
+    df = pd.read_csv(output_filepath)
+    df = df.fillna("")
+    df["missingValues"] = df["missingValues"].astype(str).replace("99.0", "99")
+    read_as_array = df.to_dict(orient="records")
+
+    assert read_as_array == valid_array_data
 
 
 def test_get_output_filepath():
