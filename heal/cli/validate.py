df28f395ddfa7466424722c3792cffa5f23ca707
George Thomas <george42@uchicago.edu>
2025-03-10 13:19:01 -0700

    (HP-1975) set error to error.message for handled ValidationError

--- heal/cli/validate.py
+++ heal/cli/validate.py
@@ -1,5 +1,6 @@
 import click
 from cdislogging import get_logger
+from jsonschema import ValidationError
 
 from heal.vlmd.validate.validate import vlmd_validate
 
@@ -22,6 +23,11 @@ def validate(input_file):
     try:
         vlmd_validate(input_file)
         logging.info("Valid")
+    except ValidationError as err:
+        logging.error(
+            f"Error in validating and extracting dictionary from {input_file}"
+        )
+        logging.error(err.message)
     except Exception as e:
         logging.error(f"Validation error {str(e)}")
         logging.error("Invalid file")
