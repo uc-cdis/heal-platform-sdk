1b00a93ccc9e12150b2a8682613337617df18d09
George Thomas <george42@uchicago.edu>
2026-09-21 13:42:49 -0700

    (HP-2721) Add output_type option for CLI extract, allow comma-separated values, eg, 'csv, json'

--- heal/cli/extract.py
+++ heal/cli/extract.py
@@ -1,9 +1,23 @@
 import click
 from cdislogging import get_logger
 
+from heal.vlmd.config import ALLOWED_OUTPUT_TYPES
 from heal.vlmd.extract.extract import vlmd_extract
 
 logging = get_logger("__name__")
+
+
+def parse_output_type(ctx, param, value):
+    """Parse string with comma separated values into list"""
+    if value:
+        output_types = [v.strip() for v in value.split(",")]
+        invalid = set(output_types) - set(ALLOWED_OUTPUT_TYPES)
+        if invalid:
+            raise click.BadParameter(
+                f"Invalid type {invalid}. Should be in {ALLOWED_OUTPUT_TYPES}"
+            )
+        return output_types
+    return value
 
 
 @click.command()
@@ -32,12 +46,21 @@ logging = get_logger("__name__")
 @click.option(
     "--output_dir",
     "output_dir",
-    help="directory to write converted dictionary'",
+    help="directory to write converted dictionary",
     default=".",
     type=click.Path(writable=True),
     show_default=True,
 )
-def extract(input_file, title, file_type, output_dir):
+@click.option(
+    "--output_type",
+    "output_type",
+    help="File type(s) for extracted dictionary. Single value (csv), or comma separated values ('csv,json')",
+    default="json",
+    type=str,
+    callback=parse_output_type,
+    show_default=True,
+)
+def extract(input_file, title, file_type, output_dir, output_type):
     """Extract HEAL-compliant VLMD file from input file"""
 
     logging.info(f"Extracting VLMD from {input_file}")
@@ -48,6 +71,7 @@ def extract(input_file, title, file_type, output_dir):
             title=title,
             file_type=file_type,
             output_dir=output_dir,
+            output_type=output_type,
         )
     except Exception as e:
         logging.error(f"Extraction error {str(e)}")
